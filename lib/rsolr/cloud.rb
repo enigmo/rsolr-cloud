949cba6382935556d1811e91950066d5426570e5
Kimura <kimura@enigmo.co.jp>
2015-07-30 14:14:39 +0900

    bundle gem rsolr-cloud

---
+++ lib/rsolr/cloud.rb
@@ -0,0 +1,7 @@
+require "rsolr/cloud/version"
+
+module Rsolr
+  module Cloud
+    # Your code goes here...
+  end
+end
---
+++ lib/rsolr/cloud/version.rb
@@ -0,0 +1,5 @@
+module Rsolr
+  module Cloud
+    VERSION = "0.0.1"
+  end
+end
